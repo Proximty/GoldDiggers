8d8088626f7a8cea0aa95038a0334ab456ba654c
Ella Marije <fungirl2050@gmail.com>
2025-11-14 15:38:28 +0100

    dusting and stone changes

--- Assets/Devs/Ella/rock1.cs
+++ Assets/Devs/Ella/rock1.cs
@@ -7,13 +7,34 @@ public interface IBreakable
 }
 public class rock1 : MonoBehaviour, IBreakable
 {
-    [SerializeField] private GameObject dirt;
-    [SerializeField] private GameObject gem;
+    [SerializeField] private GameObject _dirt;
+    [SerializeField] private int _stageRock = 1;
     public void Break()
     {
-        Debug.Log("The rock breaks");
-        dirt.SetActive(true);
-        gem.SetActive(true);
-        Destroy(this.gameObject);
+        switch (_stageRock)
+        {
+            case 1:
+                SetTransfromOfRock(0.8480377f);
+                _stageRock++;
+                break;
+            case 2:
+                SetTransfromOfRock(0.5927531f);
+                _stageRock++;
+                break;
+            case 3:
+                Debug.Log("The rock breaks");
+                _dirt.SetActive(true);
+                Destroy(this.gameObject);
+                break;
+        }
+                
+    }
+
+    //fucntion to change the size of the rock and the y rotation
+    private void SetTransfromOfRock(float size)
+    {
+        this.gameObject.transform.localScale = new Vector3(size, size, size);
+        //this.gameObject.transform.eulerAngles = new Vector3 (0, rotation, 0);
+
     }
 }

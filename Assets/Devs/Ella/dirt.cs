e8a2ff3b692f68960bcf76eaec0a80e3188fb92d
proximty <hanna@versluizen.nl>
2025-11-14 15:41:56 +0100

    rebase

--- Assets/Devs/Ella/dirt.cs
+++ Assets/Devs/Ella/dirt.cs
@@ -1,3 +1,4 @@
+using UnityEditor.SceneManagement;
 using UnityEngine;
 
 public interface IBrushable
@@ -6,11 +7,37 @@ public interface IBrushable
 }
 public class dirt : MonoBehaviour,IBrushable
 {
-    [SerializeField] private GameObject RockInfoText;
+    [SerializeField] private GameObject _gem;
+    [SerializeField] private GameObject _stoneInfo;
+    [SerializeField] private int _stageDirt = 1;
     public void Brush()
     {
-        Debug.Log("U removed the dirt from the rock");
-        RockInfoText.SetActive(true);
-        Destroy(this.gameObject);
+
+        switch (_stageDirt)
+        {
+            case 1:
+                SetTransfromOfRock(0.3f);
+                _stageDirt++;
+                break;
+            case 2:
+                SetTransfromOfRock(0.1f);
+                _stageDirt++;
+                break;
+            case 3:
+                Debug.Log("U removed the dirt from the rock");
+                _gem.SetActive(true);
+                _stoneInfo.SetActive(true);
+                Destroy(this.gameObject);
+                break;
+        }
+        
     }
+    private void SetTransfromOfRock(float size)
+    {
+        this.gameObject.transform.localScale = new Vector3(size, size, size);
+        //this.gameObject.transform.eulerAngles = new Vector3 (0, rotation, 0);
+
+    }
+
+
 }

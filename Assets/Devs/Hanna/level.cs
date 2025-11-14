aaea3f7a69eaedfce3964d32c3e702d0f529e439
proximty <hanna@versluizen.nl>
2025-11-14 14:04:49 +0100

    mappen

---
+++ Assets/Devs/Hanna/level.cs
@@ -0,0 +1,32 @@
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class level : MonoBehaviour
+{
+    public  bool _Allminerals = false;
+    [SerializeField] private GameObject _UINextLevel;
+    [SerializeField] private gamemanager _Manager;
+ 
+
+    /// <summary>
+    /// after the check it load the new level 
+    /// </summary>
+    public void Stones() 
+    {
+        _Manager._SpawnMinerals(Random.Range());
+        if (_Allminerals == true) 
+        {
+           _UINextLevel.SetActive(true);
+        }
+    }
+    /// <summary>
+    /// this makes the scene changes
+    /// </summary>
+    public void NextScene()
+    {
+        SceneManager.LoadScene("");
+    }
+}
+
+

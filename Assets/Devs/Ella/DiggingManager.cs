5d7df317f283cf1aaa209a6df4c912c02d550a60
Ella Marije <fungirl2050@gmail.com>
2025-11-19 16:46:13 +0100

    o7ikuj6yh5tr

--- Assets/Devs/Ella/DiggingManager.cs
+++ Assets/Devs/Ella/DiggingManager.cs
@@ -2,21 +2,49 @@ using System.Threading;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
+
 public class DiggingManager : MonoBehaviour
 {
+    [Header("Breaking Rocks")]
     public static bool AxeActive = false;
     [SerializeField] private rock1 _rockScipt;
     [SerializeField] private float _timer;
     [SerializeField] private GameObject _axePrefab;
     [SerializeField] private BrushManager _brushManagerScript;
-   
 
-    private void Update()
+    //[Header("Input")]
+    //[SerializeField] private Vector2 screenPos;
+
+    void Update()
     {
-        //update digging timer for animation
+
         _timer += Time.deltaTime;
-        
+
+
+        // Touch input
+        if ((Touchscreen.current != null && Touchscreen.current.primaryTouch.press.isPressed))
+        {
+            Debug.Log("Touch detected");
+
+            //screenPos = Touchscreen.current.primaryTouch.position.ReadValue();
+
+        }
+        if (Mouse.current != null && Mouse.current.leftButton.isPressed)
+        {
+            //Debug.Log("Mouse Detected");
+            //Debug.Log(Mouse.current.position.ReadValue());
+            Debug.Log(
+            Camera.main.ScreenPointToRay(Input.mousePosition));
+
+            
+
+            
+
+
+
+        }
     }
+
 
     public void enableAxe()
     {
@@ -28,7 +56,7 @@ public class DiggingManager : MonoBehaviour
     private void OnMouseDown()
     {
         Debug.Log("Plz Dig");
-        if(AxeActive == true && _timer >=0.3f)
+        if (AxeActive == true && _timer >= 0.3f)
         {
             _rockScipt.Break();
             //activate axe anim
@@ -39,6 +67,13 @@ public class DiggingManager : MonoBehaviour
             this.gameObject.transform.position = new Vector3(_objTransform.x + 0.2f, _objTransform.y + 0.2f, _objTransform.z);
             _timer = 0f;
         }
+
+    }
+
+    private void OnDrawGizmos()
+    {
         
     }
+
+
 }

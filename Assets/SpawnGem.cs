328807903fc201d368a4f279e834cfdc97a4135b
Ella Marije <fungirl2050@gmail.com>
2025-11-20 14:29:56 +0100

    busy w spawning random fossils

---
+++ Assets/SpawnGem.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public class SpawnGem : MonoBehaviour
+{
+    //string for info maken
+    //find the tmp and set the text to the string
+    public string FossilInfo;
+
+    private void Start()
+    {
+        //set rotation of object
+        //this.gameObject.transform.rotation = 
+    }
+}

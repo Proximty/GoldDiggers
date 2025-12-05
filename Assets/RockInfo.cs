353dee43167b0e863bc267abe03c961ee02c4ac6
proximty <hanna@versluizen.nl>
2025-12-05 13:43:16 +0100

    rock info error fixeds

--- Assets/RockInfo.cs
+++ Assets/RockInfo.cs
@@ -5,36 +5,36 @@ public class RockInfo : MonoBehaviour
     public string fossilName;
     [SerializeField] RockScriptManager _rockManegrScipt;
 
-    private void Start()
-    {
-        _rockManegrScipt = GameObject.Find("RockManger").GetComponent<RockScriptManager>();
-        switch (this.gameObject.name)
-        {
-            case "pile 1":
-                fossilName = _rockManegrScipt.pile1;   
-                break;
-            case "pile 2":
-                fossilName = _rockManegrScipt.pile2;
-                break ;
-            case "pile 3":
-                fossilName = _rockManegrScipt.pile3;
-                break ;
+    //private void Start()
+    //{
+    //    //_rockManegrScipt = GameObject.Find("RockManger").GetComponent<RockScriptManager>();
+    //    switch (this.gameObject.name)
+    //    {
+    //        case "pile 1":
+    //            fossilName = _rockManegrScipt.pile1;   
+    //            break;
+    //        case "pile 2":
+    //            fossilName = _rockManegrScipt.pile2;
+    //            break ;
+    //        case "pile 3":
+    //            fossilName = _rockManegrScipt.pile3;
+    //            break ;
 
-            case "pile 4":
-                fossilName = _rockManegrScipt.pile4;
-                break ;
-            case "pile 5":
-                fossilName = _rockManegrScipt.pile5;
-                break ;
-            case "pile 6":
-                fossilName = _rockManegrScipt.pile6;
-                break;
-            case "pile 7 ":
-                fossilName = _rockManegrScipt.pile7;
-                break ;
-            case "pile 8":
-                fossilName = _rockManegrScipt.pile8;
-                break;
-        }
-    }
+    //        case "pile 4":
+    //            fossilName = _rockManegrScipt.pile4;
+    //            break ;
+    //        case "pile 5":
+    //            fossilName = _rockManegrScipt.pile5;
+    //            break ;
+    //        case "pile 6":
+    //            fossilName = _rockManegrScipt.pile6;
+    //            break;
+    //        case "pile 7 ":
+    //            fossilName = _rockManegrScipt.pile7;
+    //            break ;
+    //        case "pile 8":
+    //            fossilName = _rockManegrScipt.pile8;
+    //            break;
+    //    }
+    //}
 }

d66dba1148b45d1d9188dba3f51cf8fc99f36108
proximty <hanna@versluizen.nl>
2025-12-08 10:34:58 +0100

    help icon flicker done

--- Assets/Devs/Hanna/pick up/HelpFlicker.cs
+++ Assets/Devs/Hanna/pick up/HelpFlicker.cs
@@ -1,34 +1,25 @@
 using UnityEngine;
+using UnityEngine.UI;
 
-public class HelpFlicker : MonoBehaviour
+public class ButtonFadeFlicker : MonoBehaviour
 {
-    public Flicker Flicker;
-    private float timer = 0f;
-    private float flickerDelay = 300f; // 5 minutes
-    
+    public Button targetButton;
+    public float speed = 3f;
+
+    private Image btnImage;
+
     void Start()
     {
-        Flicker = GameObject.Find("Flicker").GetComponent<Flicker>();
+        btnImage = targetButton.GetComponent<Image>();
     }
 
-    public void StartFlicker()
+    void Update()
     {
-        // Count up
-        timer += Time.deltaTime;
-
-        // When timer reaches 5 minutes, trigger flicker and reset
-        if (timer >= flickerDelay)
-        {
-            timer = 0f; // reset timer
-
-            if (Flicker != null)
-            {
-                Flicker.flicker();
-            }
-            else
-            {
-                Debug.LogWarning("Flicker component not found.");
-            }
-        }
+        float alpha = (Mathf.Sin(Time.time * speed) + 1f) / 2f; // 0-1 range
+        Color c = btnImage.color;
+        c.a = alpha;
+        btnImage.color = c;
     }
 }
+
+

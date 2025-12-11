252133738e6f69a8eadee95891d471493f4162cb
Ella Marije <fungirl2050@gmail.com>
2025-12-11 14:33:50 +0100

    garry textures changes

--- Assets/PlayAudioFromList.cs
+++ Assets/PlayAudioFromList.cs
@@ -1,13 +1,20 @@
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayAudioFromList : MonoBehaviour
 {
     [SerializeField] private Stonerarity stoneScript;
     [SerializeField] private List<AudioSource> audioX = new List<AudioSource>();
+
+    [SerializeField] private List<Texture> spriteX = new List<Texture>();
+    [SerializeField] private RawImage garrySprite;
+
     void Start()
     {
         audioX[stoneScript.index].Play();
+        garrySprite.texture = spriteX[stoneScript.index];
+        
     }
 
     
